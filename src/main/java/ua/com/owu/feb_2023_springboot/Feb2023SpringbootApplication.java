65aa0c42ec27c1993e7fb3916f36742547dafd24
VrublevskiyVolodymyr <115405839+VrublevskiyVolodymyr@users.noreply.github.com>
2023-03-27 22:52:22 +0300

    init

---
+++ src/main/java/ua/com/owu/feb_2023_springboot/Feb2023SpringbootApplication.java
@@ -0,0 +1,13 @@
+package ua.com.owu.feb_2023_springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Feb2023SpringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Feb2023SpringbootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ua/com/owu/feb_2023_springboot/Feb2023SpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.com.owu.feb_2023_springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Feb2023SpringbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
